cb56f8ff8513af558ccc50f0607da5bbc75dcbdc
banteg <4562643+banteg@users.noreply.github.com>
2025-03-19 21:29:40 +0400

    test(parser): even clearer test

--- tests/data/micro_script.rpy
+++ tests/data/micro_script.rpy
@@ -7,17 +7,17 @@ label start:
         menu:
             "• choice_1" if condition_1 == False:
             "• choice_2" if condition_2 == False:
-                voice "response_audio_1"
+                voice "narrator_audio_3"
                 n "narrator_line_3"
             "• choice_3" if condition_3 == False:
                 label nested_sequence:
-                    voice "response_audio_2"
+                    voice "narrator_audio_4"
                     n "narrator_line_4"
                     menu:
                         "• nested_choice_1" if can_proceed:
-                            voice "nested_audio_1"
+                            voice "narrator_audio_5"
                             n "narrator_line_5"
                         "• nested_choice_2":
-                            voice "nested_audio_2"
+                            voice "narrator_audio_6"
                             n "narrator_line_6"
                         "• nested_choice_3":
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -3,76 +3,62 @@ from princess.parser import Choice, Dialogue, grammar, ChoicesTransformer, clean
 
 SCRIPT = clean_script("tests/data/micro_script.rpy")
 
+DIALOGUE = [
+    Dialogue(line=3, character="n", text="narrator_line_1", voice="narrator_audio_1"),
+    Dialogue(line=5, character="n", text="narrator_line_2", voice="narrator_audio_2"),
+    Dialogue(line=11, character="n", text="narrator_line_3", voice="narrator_audio_3"),
+    Dialogue(line=15, character="n", text="narrator_line_4", voice="narrator_audio_4"),
+    Dialogue(line=19, character="n", text="narrator_line_5", voice="narrator_audio_5"),
+    Dialogue(line=22, character="n", text="narrator_line_6", voice="narrator_audio_6"),
+]
+
 CHOICES = choices = [
     Choice(
-        line=7,
+        line=8,
         label="main_dialogue",
-        choice="choice_1",
+        choice="• choice_1",
         condition="condition_1 == False",
-        prev_dialogue=[
-            Dialogue(line=2, character="n", text="narrator_line_1", voice="narrator_audio_1"),
-            Dialogue(line=4, character="n", text="narrator_line_2", voice="narrator_audio_2"),
-        ],
+        prev_dialogue=[DIALOGUE[0], DIALOGUE[1]],
         next_dialogue=[],
     ),
     Choice(
-        line=10,
+        line=9,
         label="main_dialogue",
-        choice="choice_2",
+        choice="• choice_2",
         condition="condition_2 == False",
-        prev_dialogue=[
-            Dialogue(line=2, character="n", text="narrator_line_1", voice="narrator_audio_1"),
-            Dialogue(line=4, character="n", text="narrator_line_2", voice="narrator_audio_2"),
-        ],
-        next_dialogue=[
-            Dialogue(line=11, character="n", text="narrator_line_3", voice="response_audio_1")
-        ],
+        prev_dialogue=[DIALOGUE[0], DIALOGUE[1]],
+        next_dialogue=[DIALOGUE[2]],
     ),
     Choice(
-        line=14,
+        line=12,
         label="main_dialogue",
-        choice="choice_3",
+        choice="• choice_3",
         condition="condition_3 == False",
-        prev_dialogue=[
-            Dialogue(line=2, character="n", text="narrator_line_1", voice="narrator_audio_1"),
-            Dialogue(line=4, character="n", text="narrator_line_2", voice="narrator_audio_2"),
-        ],
-        next_dialogue=[
-            Dialogue(line=16, character="n", text="narrator_line_4", voice="response_audio_2")
-        ],
+        prev_dialogue=[DIALOGUE[0], DIALOGUE[1]],
+        next_dialogue=[DIALOGUE[3]],
     ),
     Choice(
-        line=20,
+        line=17,
         label="nested_sequence",
-        choice="nested_choice_1",
+        choice="• nested_choice_1",
         condition="can_proceed",
-        prev_dialogue=[
-            Dialogue(line=16, character="n", text="narrator_line_4", voice="response_audio_2")
-        ],
-        next_dialogue=[
-            Dialogue(line=21, character="n", text="narrator_line_5", voice="nested_audio_1")
-        ],
+        prev_dialogue=[DIALOGUE[3]],
+        next_dialogue=[DIALOGUE[4]],
     ),
     Choice(
         line=24,
         label="nested_sequence",
-        choice="nested_choice_2",
+        choice="• nested_choice_2",
         condition=None,
-        prev_dialogue=[
-            Dialogue(line=16, character="n", text="narrator_line_4", voice="response_audio_2")
-        ],
-        next_dialogue=[
-            Dialogue(line=25, character="n", text="narrator_line_6", voice="nested_audio_2")
-        ],
+        prev_dialogue=[DIALOGUE[3]],
+        next_dialogue=[DIALOGUE[5]],
     ),
     Choice(
         line=28,
         label="nested_sequence",
-        choice="nested_choice_3",
+        choice="• nested_choice_3",
         condition=None,
-        prev_dialogue=[
-            Dialogue(line=16, character="n", text="narrator_line_4", voice="response_audio_2")
-        ],
+        prev_dialogue=[DIALOGUE[3]],
         next_dialogue=[],
     ),
 ]
