f68a5a6e487c618b2735e16d4ed8d6bb24a360b7
banteg <4562643+banteg@users.noreply.github.com>
2025-03-24 00:32:54 +0400

    feat(cli): add voice to cli

--- src/princess/cli.py
+++ src/princess/cli.py
@@ -1,6 +1,7 @@
 import princess.parser
 import princess.choices
 import princess.characters
+import princess.voice
 
 import typer
 
@@ -8,6 +9,7 @@ app = typer.Typer()
 app.add_typer(princess.choices.app)
 app.add_typer(princess.parser.app)
 app.add_typer(princess.characters.app)
+app.add_typer(princess.voice.app)
 
 
 if __name__ == "__main__":
