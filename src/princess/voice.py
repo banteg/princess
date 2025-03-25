aded158270ff49f48b3b904ee325aa63973ac4ba
banteg <4562643+banteg@users.noreply.github.com>
2025-03-25 04:31:37 +0400

    fix(voice): max duration detection

--- src/princess/voice.py
+++ src/princess/voice.py
@@ -66,8 +66,8 @@ def sesame(text: str, context: list[Segment], output: Path, max_length: float = 
         max_audio_length_ms=int(max_length * 1000),
         sampler=sampler,
     )
-    max_samples = int(max_length * 1000) * target_sample_rate
-    if len(signal) == max_samples:
+    max_samples = max_length * target_sample_rate
+    if len(signal) >= max_samples * 0.95:
         rich.print("[red]Warning: max length reached, possibly a bad generation or truncation[/]")
     audiofile.write(output, signal, target_sample_rate)
 
